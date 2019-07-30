260cc2438a6bd6754cbcb77d806857e97a7b22a7
Madina89 <madinalinux89@gmail.com>
2019-07-30 11:35:19 -0500

    proper key added

--- Step_1/key.tf
+++ Step_1/key.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "Bastion_host" {
     key_name = "Bastion_host"
-    public_key = "${file("/ssh_keys/app_rsa.pub")}"
+    public_key = "${file("/root/ssh/app_rsa.pub")}"
 
     
 }
