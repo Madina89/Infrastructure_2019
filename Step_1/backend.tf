a92659e700c27e3d4b13a8995e5e0faaa742053a
Madina89 <madinalinux89@gmail.com>
2019-07-30 14:20:17 -0500

    upated

--- Step_1/backend.tf
+++ Step_1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-     bucket = "gitlub-april-2019"
+     bucket = "gitlab-april-2019"
      region = "eu-west-1"
-     key = "gitlab/infra/gitlab"  
+     key = "gitlab/infra" 
     }
 }
